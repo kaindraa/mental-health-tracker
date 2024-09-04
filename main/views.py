88062ea425a625f2253b717fea79aaf38e70c0c0
kaindraa <143985776+kaindraa@users.noreply.github.com>
2024-09-04 15:56:35 +0700

    mengganti identitas di html

--- main/views.py
+++ main/views.py
@@ -3,9 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
-        'npm' : '2306123456',
-        'name': 'Pak Bepe',
-        'class': 'PBP E'
+        'npm' : '2306274964',
+        'name': 'Kaindra Rizq Sachio',
+        'class': 'PBP F'
     }
 
     return render(request, "main.html", context)
